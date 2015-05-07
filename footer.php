ee286b66612f13ea8d0c7475e5f609e71bffce1e
DinosaureMuffin <noelbilleau@gmail.com>
2015-05-07 19:28:59 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+					<div class= "footer">
+						<ul>
+							<li>Aucun chat n'a été maltraité ! </li>
+							<li><a href="contact.html">Contact</a></li>
+							<li><a href="mention.html">Mentions légales</a></li>
+						</ul>
+					</div> <!-- Fin DIV footer -->
+				</footer>
+			</section>
+		</div> <!-- Fin DIV container -->
+	</body>
